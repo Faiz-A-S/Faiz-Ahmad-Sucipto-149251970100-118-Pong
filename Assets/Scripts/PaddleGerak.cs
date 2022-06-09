4bae32dc6b70943609a30f4beb453da7595a9128
Faiz-A-S <faizasucipto@gmail.com>
2022-06-10 00:45:07 +0700

    Base Gameplay
    Base Gameplay

---
+++ Assets/Scripts/PaddleGerak.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleGerak : MonoBehaviour
+{
+    public float cepatGerak;
+    public KeyCode up;
+    public KeyCode down;
+    private Rigidbody2D rig; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get input
+        //update posisi paddle
+        MoveObject(GetInput());
+    }
+
+    // get input 
+    private Vector2 GetInput(){
+        if (Input.GetKey(up))
+        { 
+            // ke atas
+            return Vector2.up * cepatGerak;
+        } 
+        if (Input.GetKey(down))
+        { 
+            // ke bawah 
+            return Vector2.down * cepatGerak;
+        } 
+        return Vector2.zero;
+    }
+
+    //update posisi paddle
+    private void MoveObject(Vector2 gerak){
+        rig.velocity = gerak; 
+    }
+}
